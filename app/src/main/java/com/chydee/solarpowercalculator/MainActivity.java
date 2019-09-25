c80af5e1211148d00c5078a7731fab2512032b16
Chydee <desmondmuqsit@yahoo.com>
2019-09-25 09:52:53 +0100

    reformated

--- app/src/main/java/com/chydee/solarpowercalculator/MainActivity.java
+++ app/src/main/java/com/chydee/solarpowercalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,18 +14,24 @@ public class MainActivity extends AppCompatActivity {
     //EditTexts
     private EditText avgSunlightPerDay;
     private EditText applianceName;
-    //Appliances with power rated in watts
-    private EditText applianceWattage;
+
     private EditText numberOfHrsPerDay; //Indicates the number of hours the appliance is in use on a daily basis
     private EditText applianceVolt; // for appliances with volts and amps instead of watts
     private EditText applianceAmps;
 
-    //for appliances with Horse Power (HP) instead of watts, voltage, and/or amps
-    private EditText applianceHP;
+    //for appliances with Horse Power (HP) or watts instead of voltage, and/or amps
+    //Appliances with power rated in watts
+    private EditText applianceWattage;
+
+
     //Buttons
     private Button addAppliance;
     private Button reset; // Used for clearing all fields
     private Button calculate; //To do the math
+
+
+    //Spinner
+    private Spinner ratingSpinner;
 
 
 
@@ -45,13 +52,21 @@ public class MainActivity extends AppCompatActivity {
         reset = findViewById(R.id.reset_btn);
         calculate = findViewById(R.id.calculate_btn);
 
+        //Spinner
+        ratingSpinner = findViewById(R.id.type_spinner);
+
         //Add Logic to Add An Appliance Button
         addAppliance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
+
             }
         });
     }
+
+
+    private void ratingSelected(){}
+
 }
